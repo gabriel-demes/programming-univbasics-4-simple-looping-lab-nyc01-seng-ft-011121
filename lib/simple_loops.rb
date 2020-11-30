1093f74f0eb3efc8297a3e46eb99c755c4ada8ad
Gabriel Demes <gabrieldemes@nyu.edu>
2020-11-30 21:42:28 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,6 +5,7 @@ def loop_message_five_times(message)
   while c < 5
     puts string
     c += 1
+  end
 end
 
 def loop_message_n_times(message, number)
@@ -12,6 +13,7 @@ def loop_message_n_times(message, number)
   while c < int
     puts string
     c += 1
+  end
 end
 
 def output_array(array)
@@ -19,6 +21,7 @@ def output_array(array)
   while array[c] < array.length
     puts array[c]
     c += 1
+  end
 end
 
 def return_string_array(array)
@@ -27,3 +30,5 @@ def return_string_array(array)
   while array[c] < array.length
     new_list.push(arry[c].to_s)
   new_list
+end
+end
